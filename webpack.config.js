ff6c8fb977cf9b47f5436d4322aee634a875ddc3
grullo90 <g.rullo@expandjs.com>
2017-12-22 16:03:31 +0100

    Release 1.2.0

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,7 @@ const Uglify = require('uglifyjs-webpack-plugin');
 // Exporting
 module.exports = {
     entry: './index.js',
-    output: {
-        filename: 'expand.js',
-        path: `${__dirname}/dist`
-    },
-    node: {
-        Buffer: false
-    },
-    plugins: [
-        new Uglify({compress: {warnings: false}, output: {comments: false}})
-    ]
+    output: {filename: 'expand.js', path: `${__dirname}/dist`},
+    node: {Buffer: false},
+    plugins: [new Uglify({uglifyOptions: {output: {comments: /^$/}}})]
 };
